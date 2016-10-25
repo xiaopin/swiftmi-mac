83a5246871a07ee51a3bb738f5e9eacfeea7065f
xiaopin <nhope@nhopedeiMac.local>
2016-10-25 11:16:27 +0800

    完善"源码"模块

--- swiftmi-mac/Controllers/SourceCodeCollectionViewItem.swift
+++ swiftmi-mac/Controllers/SourceCodeCollectionViewItem.swift
@@ -10,23 +10,39 @@ import Cocoa
 
 class SourceCodeCollectionViewItem: NSCollectionViewItem {
     
+    let defaultColor = NSColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)
+    
     @IBOutlet weak var wrapView: NSView!
     @IBOutlet weak var previewImageView: NSImageView!
     @IBOutlet weak var titleLabel: NSTextField!
     @IBOutlet weak var dateLabel: NSTextField!
     @IBOutlet weak var categoryLabel: NSTextField!
+    
+    override var representedObject: Any? {
+        didSet {
+            if let model = self.representedObject as? SourceCodeModel {
+                previewImageView.setImage(model.preview, placeholder: NSImage(named: "placeholder-image"))
+                titleLabel.stringValue = model.title
+                dateLabel.stringValue = Utility.formatDate(TimeInterval(model.updateTime))
+                categoryLabel.stringValue = model.categoryName
+            }
+        }
+    }
+    
+    override var highlightState: NSCollectionViewItemHighlightState {
+        didSet {
+            if  highlightState == .forSelection {
+                wrapView.layer?.backgroundColor = NSColor(red: 201.0/255.0, green: 202.0/255.0, blue: 203.0/255.0, alpha: 1.0).cgColor
+            } else {
+                wrapView.layer?.backgroundColor = defaultColor.cgColor
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         wrapView.wantsLayer = true
-        wrapView.layer?.backgroundColor = NSColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0).cgColor
-        
-        if let model = self.representedObject as? SourceCodeModel {
-            previewImageView.setImage(model.preview, placeholder: NSImage(named: "placeholder-image"))
-            titleLabel.stringValue = model.title
-            dateLabel.stringValue = Utility.formatDate(TimeInterval(model.updateTime))
-            categoryLabel.stringValue = model.categoryName
-        }
+        wrapView.layer?.backgroundColor = defaultColor.cgColor
     }
     
 }
--- swiftmi-mac/Controllers/SourceCodeViewController.swift
+++ swiftmi-mac/Controllers/SourceCodeViewController.swift
@@ -8,7 +8,10 @@
 
 import Cocoa
 
-class SourceCodeViewController: NSViewController/*, NSCollectionViewDataSource, NSCollectionViewDelegate*/ {
+class SourceCodeViewController: NSViewController, NSCollectionViewDataSource, NSCollectionViewDelegate, NSCollectionViewDelegateFlowLayout {
+    
+    let reuseIdentifier = "Cell"
+    let margin: CGFloat = 10.0
 
     @IBOutlet weak var collectionView: NSCollectionView!
     var contents = [SourceCodeModel]()
@@ -17,7 +20,13 @@ class SourceCodeViewController: NSViewController/*, NSCollectionViewDataSource, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.itemPrototype = SourceCodeCollectionViewItem(nibName: "SourceCodeCollectionViewItem", bundle: nil)
+        
+        collectionView.collectionViewLayout = NSCollectionViewFlowLayout()
+        collectionView.register(SourceCodeCollectionViewItem.self, forItemWithIdentifier: reuseIdentifier)
+        collectionView.isSelectable = true
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         loadData()
     }
     
@@ -29,8 +38,57 @@ class SourceCodeViewController: NSViewController/*, NSCollectionViewDataSource, 
                 return
             }
             self.contents.append(contentsOf: response)
-            self.collectionView.content = self.contents
+            self.collectionView.reloadData()
         }
+    }
+    
+    // MARK: - NSCollectionViewDataSource
+    
+    func numberOfSections(in collectionView: NSCollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return contents.count
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        let item = collectionView.makeItem(withIdentifier: reuseIdentifier, for: indexPath)
+        item.representedObject = contents[indexPath.item]
+        return item
+    }
+    
+    // MARK: - NSCollectionViewDelegate
+    
+    func collectionView(_ collectionView: NSCollectionView, didSelectItemsAt indexPaths: Set<IndexPath>) {
+        guard let indexPath = indexPaths.first else {
+            return
+        }
+        if indexPath.item < 0 || indexPath.item >= contents.count {
+            return
+        }
+        collectionView.deselectItems(at: indexPaths)
+        
+        let content = contents[indexPath.item]
+        Utility.showWebViewController(content.url, title: content.title)
+    }
+    
+    // MARK: - NSCollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> NSSize {
+        return NSSize(width: 300.0, height: 440.0)
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return margin
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return margin
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, insetForSectionAt section: Int) -> EdgeInsets {
+        return EdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
     }
     
 }
